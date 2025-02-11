089bed582cb19f03c1521a9b7ede67f619db8e6f
DOPS5 <dzakyabdullah14@gmail.com>
2025-02-11 22:20:03 +0700

    SENDI GG

--- soal4_dzaky.py
+++ soal4_dzaky.py
@@ -4,7 +4,7 @@ def run():
     st.title("Soal 4")
     st.write("Ini adalah konten untuk Soal 4.")
     st.write("Silakan edit konten ini sesuai kebutuhan.")
-
+    st.write("PERKENALKAN NAMA SAYA ALIANDO DAN SAYA MANUSIA TERGANTENG DIDUNIA")
 # Jika dijalankan langsung (bukan dari import)
 if __name__ == "__main__":
     run()
